11227ce4639370558a73388d4cd24f04584a89f3
aayush-kalia <147748535+aayush-kalia@users.noreply.github.com>
2024-10-10 23:08:54 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
